5b219d8bd294fa7f0245713c3e0d2841198698bd
Leonardo <leonardoandre320@gmail.com>
2026-02-10 12:34:27 -0300

    Iniciado a pagina principal

--- index.php
+++ index.php
@@ -1,2 +1,149 @@
 <?php
-echo '<p>teste git<p>';
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Video</title>
+
+
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header>
+        <h2 id="lbl_title_page">Web Video</h2>
+
+        <input type="text" id="txt_main_search">
+
+        <button id="btn_main_search">Buscar</button>
+
+        <button id="btn_main_login">Login</button>
+    </header>
+
+    <main>
+        <h2>Mais recentes:</h2>
+        <div class="video_preview">
+
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <h2>Mais vistos:</h2>   
+        <div class="video_preview">
+
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+            <div class="video_preview_block">
+                <img class="img_video_preview" src="https://picsum.photos/id/237/200/300">
+                <div class="pnl_video_video_info">
+                    <p class="lbl_video_name">Teste de video</p>
+                    <p class="lbl_video_view_count">12:34</p>
+                </div>
+
+            </div>
+
+        </div>
+    </main>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
