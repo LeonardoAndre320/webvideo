039b8012719f834a0a8e2b3f89cf700de5567bbe
Leonardo <leonardoandre320@gmail.com>
2026-02-13 11:21:49 -0300

    Adicionado as telas de login e login/code

---
+++ login/code/index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="/styles/main.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <main>
+        <h3>Insira o código enviado para google@gmail.com</h3>
+        <br>
+        <input id="txt_code" type="text" placeholder="Código" >
+        <br>
+        <button id="btn_continue">Continuar</button>
+    </main>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ login/index.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="/styles/main.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <main>
+        <button id="btn_login">Login</button>
+        <button id="btn_register">Cadrastrar</button>
+        <br>
+        <input id="txt_email" type="text" placeholder="E-mail" >
+        <br>
+        <input id="txt_password" type="password" placeholder="Senha" >
+        <br>
+        <button id="btn_continue">Continuar</button>
+    </main>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
